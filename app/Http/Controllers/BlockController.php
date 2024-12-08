2f84b0ab19a01c418472f700925311f4f8f9f0fc
Glovejuggler <36526137+Glovejuggler@users.noreply.github.com>
2024-12-08 21:49:01 +0800

    initial

---
+++ app/Http/Controllers/BlockController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Block;
+use Illuminate\Http\Request;
+
+class BlockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return inertia('Blocks/Index', [
+            'blocks' => Block::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'control_number' => 'required',
+            'block_number' => 'required',
+            'name' => 'required'
+        ]);
+
+        Block::create($request);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Block $block)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Block $block)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Block $block)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Block $block)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Blocks/Index.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Head, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import Modal from '@/Components/Modal.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const createBlockModal = ref(false)
+const newBlock = useForm({
+    name: '',
+    control_number: '',
+    block_number: '',
+})
+
+const props = defineProps({
+    blocks: Object,
+})
+</script>
+
+<template>
+    <Head title="Lots"/>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <i @click="createBlockModal = true" class="bx bx-plus inline-flex h-8 w-8 justify-center items-center dark:text-white hover:bg-black/10 dark:hover:bg-white/10 cursor-pointer rounded-full"></i>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            </div>
+        </div>
+    </div>
+
+    <!-- Create Block Modal -->
+    <Modal :show="createBlockModal" closeable="false">
+        <div class="p-4 dark:text-white">
+            <InputLabel for="name" value="Block name"/>
+            <TextInput id="name" type="text" class="mt-1 mb-4 block w-full" v-model="newBlock.name"/>
+
+            <InputLabel for="control_number" value="Control number"/>
+            <TextInput id="control_number" type="text" class="mt-1 mb-4 block w-full" v-model="newBlock.control_number"/>
+
+            <InputLabel for="block_number" value="Block number"/>
+            <TextInput id="block_number" type="text" class="mt-1 block w-full" v-model="newBlock.block_number"/>
+
+            
+        </div>
+    </Modal>
+</template>
